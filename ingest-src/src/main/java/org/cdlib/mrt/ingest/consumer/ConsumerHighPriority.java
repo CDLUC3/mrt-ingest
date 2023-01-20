7b638b258119bebb8437e39efd5599ca23ab8e40
Mark Reyes <mreyes@uc3-mrtdocker03x2-dev.cdlib.org>
2023-01-20 14:27:14 -0800

    Fix latent bug in original Consumer Daemon. With HP daemon this will lead to "too many open file" error condition

--- ingest-src/src/main/java/org/cdlib/mrt/ingest/consumer/ConsumerHighPriority.java
+++ ingest-src/src/main/java/org/cdlib/mrt/ingest/consumer/ConsumerHighPriority.java
@@ -465,10 +465,6 @@ class ConsumerDaemonHighPriority implements Runnable
 
 	    Item item = null;
 	    try {
-		// reconnect may be necessary
-                zooKeeper = new ZooKeeper(queueConnectionString, DistributedQueue.sessionTimeout, new Ignorer(), sessionID, sessionAuth);
-                distributedQueue = new DistributedQueue(zooKeeper, queueNode, null);
-
 	        item = distributedQueue.updateStatus(id, Item.CONSUMED, Item.PENDING);
 	    } catch (SessionExpiredException see) {
 	        System.err.println("[error]" + MESSAGE +  "Session expired.  Attempting to recreate session while requeueing.");
