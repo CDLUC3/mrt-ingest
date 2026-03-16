b5f61e88a5a6e414d6c9b96080355c53451748c3
mreyescdl <skidly_2@yahoo.com>
2026-03-16 12:04:52 -0700

    Make manifest digest value case-insensitive

--- ingest-src/src/main/java/org/cdlib/mrt/ingest/handlers/initialize/HandlerVerify.java
+++ ingest-src/src/main/java/org/cdlib/mrt/ingest/handlers/initialize/HandlerVerify.java
@@ -105,8 +105,10 @@ public class HandlerVerify extends Handler<JobState>
 
 	    String calculatedChecksum = message.getChecksum();
 
-	    if (! calculatedChecksum.equals(value)) {
+	    if (! calculatedChecksum.equalsIgnoreCase(value)) {
 		throw new TException.FIXITY_CHECK_FAILS("[error] submission package checksum mismatch: " + submissionPackage.getName());
+	    } else {
+	    	System.out.println("[info]: " + MESSAGE + "HandlerVerify: Digest value matches: " + submissionPackage.getName());
 	    }
 
             // metadata file in ANVL format
